9e57cf7b4650c90a23e145ff85ef0e710226d483
Avinash Thakur <iamavinashthakur.at@gmail.com>
2025-04-04 02:07:59 +0530

    fix: sample json linked to package.json

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
+const packageConfig = require("./package.json");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
   assetPrefix: process.env.NEXTJS_ASSET_PREFIX || "",
+  env: {
+    SAMPLE_JSON: JSON.stringify(packageConfig, null, 2),
+  },
 };
 
 module.exports = nextConfig;
